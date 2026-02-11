dc6faf635d8aa59e8d5a3386d58931ddc598094f
dannapalolacr-commits <dannapalolacr@gmail.com>
2026-02-10 18:20:33 -0600

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-function romperHuevo() {
-    const container = document.querySelector('.egg-container');
-    
-    // Si ya está abierto, no hacer nada
-    if (container.classList.contains('open')) return;
-
-    // Añadir la clase que separa las piezas y muestra la imagen
-    container.classList.add('open');
-}
